7d4b55a8aee8042839db6caa5cc1160d09a81872
limhannah <92508016+limhannah@users.noreply.github.com>
2025-07-17 10:42:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'c237-boss.mysql.database.azure.com',
+    user: 'c237boss',
+    password: 'c237boss!',
     database: 'c237_supermarketdb'
   });
 
